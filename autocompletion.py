12627a13e0b9978cf0b600b410c7fceac221e033
danielwagner <daniel.wagner@1und1.de>
2013-05-02 10:13:16 +0200

    use settings file; load api data from multiple indices

--- autocompletion.py
+++ autocompletion.py
@@ -4,34 +4,60 @@ import json
 import urllib
 import os
 
-DEBUG = False
-API_URL = "http://demo.qooxdoo.org/current/apiviewer/script/apiindex.json"
-INDEX_FILE = "apiindex.json"
-
 
 class QxAutoCompleteCommand(sublime_plugin.EventListener):
     def __init__(self):
+        self.settings = sublime.load_settings("qooxdoo.sublime-settings")
+        self.debug = self.settings.get("autocomplete_debug")
+
         self.apidata = self.getData()
 
     def getData(self):
-        if os.path.isfile(INDEX_FILE):
-            if DEBUG:
-                print "Loading API data from file"
-            fileObj = open(INDEX_FILE)
-            data = json.load(fileObj)
-        else:
-            if DEBUG:
-                print "Loading API data from URL"
-            data = self.loadData()
-            fileObj = open(INDEX_FILE, "w+")
-            json.dump(data, fileObj)
+        data = {}
+        apiPaths = self.settings.get("autocomplete_api_paths")
+        for lib, path in apiPaths.iteritems():
+            libData = None
+            cachePath = os.path.abspath("api_cache")
+            indexFile = os.path.join(cachePath, "apiindex_" + lib + ".json")
+
+            if os.path.isfile(indexFile):
+                if self.debug:
+                    print "Loading cached %s API data from file %s" % (lib, indexFile)
+                fileObj = open(indexFile)
+                libData = json.load(fileObj)
+            else:
+                if os.path.isfile(path):
+                    if self.debug:
+                        print "Collecting %s API data from file system path %s" % (lib, path)
+                    libData = self.loadDataFromFile(path)
+                else:
+                    if self.debug:
+                        print "Collecting %s API data from URL %s" % (lib, path)
+                    libData = self.loadDataFromUrl(path)
+
+                if libData:
+                    if self.debug:
+                        print "Writing %s API data cache to file %s" % (lib, indexFile)
+                    if not os.path.isdir(cachePath):
+                        os.makedirs(cachePath)
+                    fileObj = open(indexFile, "w+")
+                    json.dump(libData, fileObj)
+
+            data[lib] = libData
 
         return data
 
-    def loadData(self):
-        indexFile = urllib.urlopen(API_URL)
+    def loadDataFromUrl(self, url):
+        indexFile = urllib.urlopen(url)
         index = indexFile.read()
         index = json.loads(index)
+        data = index["__fullNames__"]
+
+        return data
+
+    def loadDataFromFile(self, path):
+        indexFile = open(path)
+        index = json.load(indexFile)
         data = index["__fullNames__"]
 
         return data
@@ -48,14 +74,15 @@ class QxAutoCompleteCommand(sublime_plugin.EventListener):
         lineText = lineText.strip().rsplit(" ")[-1]
 
         result = []
-        for className in self.apidata:
-            if className.startswith(lineText):
-                completion = prefix + className[len(lineText):]
-                if len(prefix) == 1 and prefix[0].isupper():
-                    completion = className
-                if DEBUG:
-                    print "prefix: %s, lineText: %s, className %s, completion: %s" % (prefix, lineText, className, completion)
-                result.append((className, completion))
+        for lib in self.apidata:
+            for className in self.apidata[lib]:
+                if className.startswith(lineText):
+                    completion = prefix + className[len(lineText):]
+                    if len(prefix) == 1 and prefix[0].isupper():
+                        completion = className
+                    if True or self.debug:
+                        print "prefix: %s, lineText: %s, className %s, completion: %s" % (prefix, lineText, className, completion)
+                    result.append((className, completion))
 
         if len(result) > 0:
             return (result, sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
